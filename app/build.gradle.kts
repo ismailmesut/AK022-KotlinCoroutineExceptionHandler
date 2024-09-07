c34a1a9f7509030193d87aa821199d6e1e6d102a
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-07 10:16:14 +0300

    updated: coroutine exception handler

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.5")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.5")
 }
--- app/src/main/java/com/ismailmesutmujde/kotlincoroutineexceptionhandler/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlincoroutineexceptionhandler/MainActivity.kt
@@ -2,6 +2,13 @@ package com.ismailmesutmujde.kotlincoroutineexceptionhandler
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.supervisorScope
 
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +16,49 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        // Coroutinelerde hatayı yakalamanın doğru yolu; CoroutineExceptionHandler kullanmaktır.
+
+        val handler = CoroutineExceptionHandler { coroutineContext, throwable ->
+            println("exception: " + throwable.localizedMessage)
+        }
+        /*
+        lifecycleScope.launch(handler) {
+            throw Exception("error")
+        }
+
+         */
+        /*
+        lifecycleScope.launch(handler) {
+            launch {
+                throw Exception("error2")
+            }
+            launch {
+                delay(500L)
+                println("this is executed")
+            }
+        }
+         */
+
+
+        // Birden fazla launch kullanıyorsak; supervisorScope kullanmak gerekir.
+        lifecycleScope.launch(handler) {
+            supervisorScope {
+                launch {
+                    throw Exception("error2")
+                }
+                launch {
+                    delay(500L)
+                    println("this is executed")
+                }
+            }
+        }
+
+
+        CoroutineScope(Dispatchers.Main + handler).launch {
+            launch {
+                throw Exception("error in a coroutine scope")
+            }
+        }
     }
 }
